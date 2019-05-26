b04100eadc62c2a01ec78b2855ea44c1ae1f3dc7
jhonrsalcedo <jhonfor@gmail.com>
2019-05-26 01:24:19 -0500

    added functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Search from './components/Search';
+
 
 
 class Apps extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      query: '',
+      images:''
+    }
+  }
+
+  //Metodo para realizar la consulta del api img
+  checkApi = async() => {
+    //comprobar que esta funcionando el callback
+    //console.log("desde consultar api");
+
+    //llamamos al dato de busqueda este viene del state
+    //lo guardamos en una variable, podemos utilizar el mismo nombre
+    const query = this.state.query;
+    const url = `https://pixabay.com/api/?key=12592538-85b554429303781a885b89c56&q=${query}&per_page=30`;
+    //comprobamos que image se esta pasando el url 
+    //console.log(url)
+
+    //realizamos la consulta con axios 
+    try {
+      const response = await axios.get(url);
+      //para comprabar que estamos recibiendo bien los datos de la Api ejecutiamos un console.log()
+      console.log(response.data.hits) 
+
+      //luego de pasarla al state podemos tomar images y pasarla por otro componente, este servira para dibujar en pantalla lo que estamos buscando.
+      this.setState({
+        images: response.data.hits
+      })
+    } catch (error) {
+      console.error(`Hay un error ${error}`)
+
+    }
+
+  }
+
+  //metodo para traer los datos del formulario
+  dataSearch = (query) => {
+    //comprobar que se esta pasando por state
+    //console.log(image)
+
+    //lo pasamos al state de Apps
+    //utilizamos un callback (checkApi) este se ejecutara despues de que suba los datos al state
+    this.setState({
+      query
+    }, () =>{
+      this.checkApi()
+    } )
   }
   render() {
     return (
       <div className="app container">
         <div className="jumbotron">
           <p className="lead text-center"><b>Buscador de Imágenes</b></p>
-          <Search />
+          <Search
+            dataSearch={this.dataSearch}
+          />
         </div>
 
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-        constructor(props){
-            super(props);
-            this.state={
-                searchValue:''
-            }
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkValue: ''
         }
-    getData = (e) =>{
+    }
+    getData = (e) => {
         e.preventDefault();
         //leemos el valor
-        const termino = this.state.searchValue
-        console.log(termino)
+        const query = this.state.checkValue
+        //console.log(termino)
+
+        //lo pasamos por props para app.js
+        this.props.dataSearch(query);
 
         this.setState({
-            searchValue:''
+            checkValue: ''
         })
     }
 
-    handleData = (e) =>{
-       this.setState({
-        [e.target.name]: e.target.value
-       })
+    handleData = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
@@ -29,7 +32,7 @@ class Search extends Component {
             <form onSubmit={this.getData}>
                 <div className="row">
                     <div className="form-group col-md-8">
-                        <input className="form-control form-control-lg" type="text" placeholder="Busca tu imagen, ejemplo: Tecnología" name="searchValue" onChange={this.handleData} value={this.state.searchValue} />
+                        <input className="form-control form-control-lg" type="text" placeholder="Busca tu imagen, ejemplo: Tecnología" name="checkValue" onChange={this.handleData} value={this.state.checkValue} />
                     </div>
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar..." />
